c9091e76d4e2ac4eead41706612d7569ad49ebc7
edilbertof19 <54913985+edilbertof19@users.noreply.github.com>
2020-04-17 18:13:15 -0500

    -making sure I committed

--- src/main/java/com/CIS3368/FinalProject/controllers/BookController.java
+++ src/main/java/com/CIS3368/FinalProject/controllers/BookController.java
@@ -4,6 +4,7 @@ import com.CIS3368.FinalProject.Models.Books;
 import com.CIS3368.FinalProject.Models.BooksRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
