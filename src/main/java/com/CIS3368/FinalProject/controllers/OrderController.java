eda5cf0e6b5ea89c761bdcd88bf845b371180788
rrios4 <rogeliorios348@gmai.com>
2020-04-23 00:59:26 -0500

    - Added new information about new server in order to connect to mySQL

--- src/main/java/com/CIS3368/FinalProject/controllers/OrderController.java
+++ src/main/java/com/CIS3368/FinalProject/controllers/OrderController.java
@@ -1,14 +1,26 @@
 package com.CIS3368.FinalProject.Controllers;
 
-import com.CIS3368.FinalProject.Models.OrdersRepo;
+import com.CIS3368.FinalProject.Models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+import java.util.UUID;
+
 @Controller
 public class OrderController {
     @Autowired
     private OrdersRepo ordersRepo;
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Autowired
+    private CustomerRepo customerRepo;
 
     @RequestMapping("/orderView")
     public ModelAndView OrderView()
@@ -17,6 +29,45 @@ public class OrderController {
         orderView.addObject("orderList1", ordersRepo.findAll());
         return orderView;
     }
+
+    @RequestMapping(value = "/saveOrder", method = RequestMethod.POST)
+    public ModelAndView save(@RequestParam("orderid") String orderId, @RequestParam("employeeid") String employeeId, @RequestParam("employeefirstname") String employeeFirstName,
+                             @RequestParam("employeelastname") String employeeLastName, @RequestParam("id") String id, @RequestParam("bookid") String bookId)
+    {
+        ModelAndView ordersView = new ModelAndView("redirect:/orderView");
+        Orders orderToSave;
+        Employee employeeToSave;
+        Customer customerToSave;
+        if(!orderId.isEmpty())
+        {
+            Optional<Orders> users3 = ordersRepo.findById(orderId);
+            orderToSave = users3.get();
+        }
+        else if(!employeeId.isEmpty()){
+            Optional<Employee> users4 = employeeRepo.findById(employeeId);
+            employeeToSave = users4.get();
+
+        }
+        else if(!id.isEmpty()){
+            Optional<Customer> users5 = customerRepo.findById(id);
+            customerToSave = users5.get();
+
+        }
+        else
+        {
+            orderToSave = new Orders();
+            orderToSave.setOrderId(UUID.randomUUID().toString());
+            employeeToSave = new Employee();
+            employeeToSave.setEmployeeId(UUID.randomUUID().toString());
+            customerToSave =  new Customer();
+            customerToSave.setId(UUID.randomUUID().toString());
+        }
+
+        ordersView.addObject("orderlist", ordersRepo.findAll());
+        return ordersView;
+    }
+
+
 
 }
 
